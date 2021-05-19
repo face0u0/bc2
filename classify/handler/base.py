d9caa3751f56c7c75e80cddcfb5194d0f31095a7
face0u0 <me@face0u0.net>
2021-05-20 00:56:52 +0900

    add comment

--- classify/handler/base.py
+++ classify/handler/base.py
@@ -5,12 +5,15 @@ from classify.handler.image import ImageHandler
 from classify.entity import DestFile, SourceFile
 
 class Handler:
+    # ファイル名と中身（画像なら圧縮とか）を変換する
     def convert(self, source: SourceFile) -> DestFile:
         pass
 
+    # 自身が担当のファイル形式ならtrue
     def is_in_charge(self, source: SourceFile) -> bool:
         pass
 
+    # 保存先ディレクトリのベース部分を返す（画像なら"images/"とか）
     def save_dir(self) -> PurePath:
         pass
 
